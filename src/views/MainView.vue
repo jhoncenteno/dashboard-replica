57f961e3e4abe4f46614509f40a64df964ea3d62
Jhon <jcenteno.ing@gmail.com>
2025-04-27 17:36:46 -0400

    Project initialized

---
+++ src/views/MainView.vue
@@ -0,0 +1,14 @@
+<script setup>
+
+</script>
+
+<template>
+    
+    <h1 class="text-3xl text-blue-500 font-bold underline mb-5">
+        Main View a
+    </h1>
+</template>
+
+<style scoped>
+
+</style>
