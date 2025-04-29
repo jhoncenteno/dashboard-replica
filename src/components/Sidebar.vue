2f20dc66df6d7895df180600d78928e515ba9164
Jhon <jcenteno.ing@gmail.com>
2025-04-28 20:29:29 -0400

    Sidebar finished

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -1,11 +1,22 @@
 <script setup>
+import home from '../assets/icons/home.svg'
+import magicWand from '../assets/icons/magic-wand.svg'
+import comments from '../assets/icons/comments.svg'
+import favorite from '../assets/icons/favorite.svg'
+import autoPlay from '../assets/icons/auto-play.svg'
+import settingsIcon from '../assets/icons/settings.svg'
+
 const items = [
-    { name: 'Dashboard' },
-    { name: 'Actions' },
-    { name: 'Comments' },
-    { name: 'Favorites' },
-    { name: 'Auto Comment' }
+    { name: 'Dashboard', icon: home },
+    { name: 'Actions', icon: magicWand },
+    { name: 'Comments', icon: comments },
+    { name: 'Favorites', icon: favorite },
+    { name: 'Auto Comment', icon: autoPlay }
 ]
+
+const settings = {
+    name: 'Setttings', icon: settingsIcon
+}
 </script>
 
 <template>
@@ -17,17 +28,22 @@ const items = [
         <div class="flex-1 flex flex-col mt-8">
             <ul class="">
                 <li v-for="(item, index) in items" :key="index">
-                    <span class="block px-8 py-3 w-full" :class="{
+                    <span class="flex items-center gap-2 px-8 py-3 w-full font-semibold group" :class="{
                         'bgMain text-white': index === 0,
                         'hover:bgMain hover:text-white': index !== 0
                     }">
+                        <img :src="item.icon" alt="" class="w-6 group-hover:invert"
+                            :class="{ 'invert': index === 0}">
                         {{ item.name }}
                     </span>
                 </li>
             </ul>
             <ul class="mt-auto border-t border-gray-300">
                 <li>
-                    <span class="block px-8 py-3 hover:bgMain hover:text-white">Settings</span>
+                    <span class="flex items-center gap-2 px-8 py-3 hover:bgMain hover:text-white font-semibold group">
+                        <img :src="settings.icon" alt="" class="w-5 group-hover:invert">
+                        {{ settings.name }}
+                    </span>
                 </li>
             </ul>
         </div>
